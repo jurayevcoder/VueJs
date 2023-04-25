9961e18a4eaceb4adc7b8d921afaa0419540649a
jurayevcoder <davlatjurayev.1@gmail.com>
2023-04-25 17:19:27 +0500

    vue.js

---
+++ module-2/lesson-2/src/components/Main.vue
@@ -0,0 +1,21 @@
+<script>
+import Section from './Section.vue'
+export default {
+    components: {
+        Section,
+    }
+};
+</script>
+
+<template>
+    <main>
+        <Section text="INTRO" color="orange" />
+        <Section text="HERO" color="dodgerblue" />
+    </main>
+</template>
+
+<style lang="scss" scoped>
+main {
+    min-height: calc(100vh - 70px - 70px);
+}
+</style>
---
+++ module-2/lesson-2/src/components/Section.vue
@@ -0,0 +1,28 @@
+<script>
+export default {
+    props: {
+        text: {
+            type: String,
+            required: true,
+        },
+        color: String,
+    }
+};
+</script>
+
+<template>
+    <section class="intro" :style="'background-color:' + color">
+        <div class="container">
+            <h1 class="text-center">{{ text }}</h1>
+        </div>
+    </section>
+</template>
+
+<style lang="scss" scoped>
+.intro {
+    min-height: 100vh;
+    display: flex;
+    justify-content: center;
+    align-items: center;
+}
+</style>
